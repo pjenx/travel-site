3a37a8fea702befdd48c81afda688ad53963ad48
Paul Jenkins <paul.pjenks@gmail.com>
2020-12-17 13:20:53 -0600

    Completed modal functionality. Split code.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,12 +3,27 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 
-let stickyHeader = new StickyHeader();
-
+new StickyHeader();
+new MobileMenu();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
+let modal;
 
-let mobileMenu = new MobileMenu();
+document.querySelectorAll('.open-modal').forEach(elem => {
+    elem.addEventListener('click', e => {
+        e.preventDefault();
+        if (!modal) {
+            import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
+                modal = new x.default();
+                modal.openTheModal();
+            }).catch(
+                () => console.log('There was a problem')
+            );
+        } else {
+            modal.openTheModal();
+        }
+    });
+})
 
 if (module.hot) {
     module.hot.accept();
